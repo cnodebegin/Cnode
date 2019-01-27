49d80fedcc306187651d3ff4c8be4da9d889d7a7
YokiBang <yoki1069@163.com>
2019-01-27 13:41:50 +0800

    13.41

---
+++ WX.Three.Api/WX.Three.Api/Controllers/CNodeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WX.Three.Api.Controllers
+{
+    public class CNodeController : ApiController
+    {
+        [HttpGet]
+        public List<Info> topics()
+        {
+            List<Info> list = new List<Info>();
+            list.Add(new Info() { ID = 1, Title = "news" });
+            return list;
+        }
+    }
+    public class Info
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+    }
+}
+
